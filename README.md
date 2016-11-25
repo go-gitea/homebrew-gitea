a77705b05760be5d67a6116d96880d3b34217187
Thomas Boerger <tboerger@suse.de>
2016-11-25 14:46:19 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# homebrew-gitea
-Homebrew repository to install Gitea on macOS
+# Gitea: Homebrew
+
+[![Join the chat at https://gitter.im/kleister/kleister](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/kleister/kleister)
+
+Homebrew repository to install Gitea on macOS.
+
+## Prepare
+
+```bash
+brew tap go-gitea/gitea
+```
+
+## Install
+
+### gitea
+
+```bash
+brew install gitea
+gitea -h
+```
+
+## Contributing
+
+Fork -> Patch -> Push -> Pull Request
+
+## Authors
+
+* [Maintainers](https://github.com/orgs/go-gitea/people)
+* [Contributors](https://github.com/go-gitea/gitea/graphs/contributors)
+
+## License
+
+This project is under the MIT License. See the [LICENSE](LICENSE) file for the
+full license text.
+
+## Copyright
+
+```
+Copyright (c) 2016 The Gitea Authors <https://gitea.io>
+```
---
+++ gitea.rb
@@ -0,0 +1,50 @@
+require "formula"
+
+class Gitea < Formula
+  homepage "https://github.com/go-gitea/gitea"
+  head "https://github.com/go-gitea/gitea.git"
+
+  stable do
+    url "http://dl.gitea.io/gitea/1.0.0/gitea-1.0.0-darwin-amd64"
+    sha256 `curl -s http://dl.gitea.io/gitea/1.0.0/gitea-1.0.0-darwin-amd64.sha256`.split(" ").first
+    version "1.0.0"
+  end
+
+  devel do
+    url "http://dl.gitea.io/gitea/master/gitea-master-darwin-amd64"
+    sha256 `curl -s http://dl.gitea.io/gitea/master/gitea-master-darwin-amd64.sha256`.split(" ").first
+    version "master"
+  end
+
+  head do
+    url "https://github.com/go-gitea/gitea.git", :branch => "master"
+
+    depends_on "go" => :build
+    depends_on "mercurial" => :build
+    depends_on "bzr" => :build
+    depends_on "git" => :build
+  end
+
+  test do
+    system "#{bin}/gitea", "--version"
+  end
+
+  def install
+    if build.head?
+      mkdir_p buildpath/File.join("src", "code.gitea.io")
+      ln_s buildpath, buildpath/File.join("src", "code.gitea.io", "gitea")
+
+      ENV.append_path "PATH", File.join(buildpath, "bin")
+
+      ENV["GOPATH"] = buildpath
+      ENV["GOHOME"] = buildpath
+      ENV["TAGS"] = "sqlite"
+
+      system("make", "build")
+
+      bin.install "#{buildpath}/bin/gitea" => "gitea"
+    else
+      bin.install "#{buildpath}/gitea-master-darwin-amd64" => "gitea"
+    end
+  end
+end
