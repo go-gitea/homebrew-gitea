c7744b52f855d51c9c1aa7c0f0b65a3becfacf74
quanta <quanta@robotinfra.com>
2017-02-12 07:26:47 +0700

    1.0.0 --> 1.0.1

--- gitea.rb
+++ gitea.rb
@@ -43,7 +43,7 @@ class Gitea < Formula
     when build.devel?
       bin.install "#{buildpath}/gitea-master-darwin-10.6-amd64" => "gitea"
     else
-      bin.install "#{buildpath}/gitea-1.0.0-darwin-10.6-amd64" => "gitea"
+      bin.install "#{buildpath}/gitea-1.0.1-darwin-10.6-amd64" => "gitea"
     end
   end
 end
